c9636d1e35f7cebaa37524735a8b818dcb61c50a
Crito-VanaheimServers <66382331+Crito-VanaheimServers@users.noreply.github.com>
2023-11-30 17:04:36 -0600

    Delete rcon directory

--- rcon/rcon_BotStatus.bat
+++
@@ -1,3 +0,0 @@
-%~dp0mcrcon.exe -H 127.0.0.1 -P 27020 -p Charlie2012 "ListPlayers">%~dp0rcon_BotStatus.txt
-for /f "usebackq tokens=* delims=" %%a in ("%~dp0rcon_BotStatus.txt") do (echo(%%a)>>~.txt
-move /y  ~.txt "%~dp0rcon_BotStatus.txt"
--- rcon/rcon_SendChat.bat
+++
@@ -1,3 +0,0 @@
-%~dp0mcrcon.exe -H 127.0.0.1 -P 27020 -p Charlie2012 "ServerChat (From Discord) crito_vgs: this is still working?">%~dp0rcon_SendChat.txt
-for /f "usebackq tokens=* delims=" %%a in ("%~dp0rcon_SendChat.txt") do (echo(%%a)>>~.txt
-move /y  ~.txt "%~dp0rcon_SendChat.txt"
